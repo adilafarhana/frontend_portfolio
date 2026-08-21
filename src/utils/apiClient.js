42a54f81dbd15f564ce626818adb9f7679d62517
Adila Farhana <adila@bpract.com>
2026-08-21 14:05:48 +0530

    chnages

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAdminToken } from "./auth";
 
 const apiBaseURL =
-  (process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000").replace(
+  (process.env.REACT_APP_API_BASE_URL ).replace(
     /\/$/,
     ""
   );
